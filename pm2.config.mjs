8cd112bff24a2cfe6d5cf79ce0f1aed0caceeedf
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-06-21 00:53:22 +0100

    Update pm2.config.mjs

--- pm2.config.mjs
+++ pm2.config.mjs
@@ -1,8 +1,9 @@
-module.exports = {
-  name: "amatullah", // Name of your application
-  script: "index.ts", // Entry point of your application
-  interpreter: "bun", // Bun interpreter
-  env: {
+export default {
+   name: "amatullah", // Name of your application
+   script: "index.ts", // Entry point of your application
+   interpreter: "bun", // Bun interpreter
+   env: {
     PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
   },
-};
+
+}
