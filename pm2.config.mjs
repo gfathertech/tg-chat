83123cfdd67c158eb5f37475abeaf22250480e02
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-06-21 00:21:44 +0100

    Pm2

--- pm2.config.mjs
+++ pm2.config.mjs
@@ -2,7 +2,7 @@ export default {
   apps: [
     {
       name: "amatullah",
-      script: "index.ts",         
+      script: "./index.ts",         
       
       interpreter: "bun",         
       watch: false
