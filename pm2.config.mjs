b25c5188ff37b899169177cb3a01a945ba4d02af
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-06-21 00:35:26 +0100

    Update pm2.config.mjs

--- pm2.config.mjs
+++ pm2.config.mjs
@@ -2,7 +2,7 @@ export default {
   apps: [
     {
       name: "amatullah",
-      script: "bun",               // Idk if I should leave this
+      script: "/bin/bash",               // Idk if I should leave this
       args: ["bun index.ts"],      // added this in args
       watch: false
     }
