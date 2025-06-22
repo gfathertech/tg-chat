174f066fa1b0661c0748490409b1255cc4358ed3
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-06-22 12:21:47 +0100

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-export interface FluxResponse {
+export interface Resp {
   BK9: string[];
 }
 
 export async function flux(query: string): Promise<string | Error> {
   try {
-    const response = await axios.get<FluxResponse>(`https://bk9.fun/ai/fluximg?q=${encodeURIComponent(query)}`);
+    const response = await axios.get<Resp>(`https://bk9.fun/ai/fluximg?q=${encodeURIComponent(query)}`);
     const res = response.data.BK9[0];
     return res;
   } catch (error) {
